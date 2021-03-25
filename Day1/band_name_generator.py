7ac307213aecfcc630dd17ee0de428c9fff544fb
Sandip <nair_sandip@hotmail.com>
2021-03-26 03:15:52 +0530

    Committing Day1 Changes

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,8 @@
+#! usr/bin/python3
+
+print("Welcome to the Band Name Generator")
+
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print(f"Name of your band could be {city} {pet}s")
